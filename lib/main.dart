856ff2072c15c3dc86661561544495a664f07493
catintheboxx <wased1009@gmail.com>
2023-11-25 01:31:26 +0700

    modify: remove mulitBlocProvider on all main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,17 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'src/app.dart';
 import 'src/core/di.dart';
 import 'src/core/env.dart';
 import 'src/core/network.dart';
-import 'src/shared/bloc_state.dart';
 
 void main() async {
   await dotenv.load(fileName: '.env');
   AppEnvironment.setupEnv(Environment.local);
   await initializeDependencies();
   HttpOverrides.global = MyHttpOverrides();
-  runApp(
-    MultiBlocProvider(
-      providers: [
-        labelBloc,
-      ],
-      child: const GetGoalApp(),
-    ),
-  );
+  runApp(const GetGoalApp());
 }
--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,24 +1,15 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'src/app.dart';
 import 'src/core/di.dart';
 import 'src/core/env.dart';
 import 'src/core/network.dart';
-import 'src/shared/bloc_state.dart';
 
 void main() async {
   AppEnvironment.setupEnv(Environment.dev);
   await initializeDependencies();
   HttpOverrides.global = MyHttpOverrides();
-  runApp(
-    MultiBlocProvider(
-      providers: [
-        labelBloc,
-      ],
-      child: const GetGoalApp(),
-    ),
-  );
+  runApp(const GetGoalApp());
 }
