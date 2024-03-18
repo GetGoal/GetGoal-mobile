0888f56aae4e631d26b455de33f335b7218c091d
catintheboxx <wased1009@gmail.com>
2024-03-18 23:17:27 +0700

    Update program info page for new design

--- lib/src/features/program/presentation/screens/program_info/widgets/task_card_widget.dart
+++ lib/src/features/program/presentation/screens/program_info/widgets/task_card_widget.dart
@@ -1,34 +1,43 @@
 import 'package:flutter/material.dart';
 
 import '../../../../../../shared/themes/color.dart';
+import '../../../../../../shared/themes/font.dart';
+import '../../../../../../shared/widgets/text/get_goal_gradient_text.dart';
 
 class TaskCard extends StatelessWidget {
-  const TaskCard({super.key, this.number, this.taskName});
+  const TaskCard({super.key, this.number, this.taskName, this.taskDesc});
 
   final int? number;
   final String? taskName;
+  final String? taskDesc;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.only(top: 8, right: 16, bottom: 8, left: 16),
+      padding: const EdgeInsets.all(16.0),
+      margin: const EdgeInsets.only(bottom: 16),
       width: double.infinity,
-      child: Row(
+      decoration: BoxDecoration(
+        color: AppColors.secondary,
+        boxShadow: AppShadow.shadow,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Container(
-            width: 40,
-            height: 40,
-            decoration:
-                BoxDecoration(shape: BoxShape.circle, color: AppColors.primary),
-            child: Center(child: Text('$number')),
+          Text(
+            'Task $number',
+            style: caption2Regular().copyWith(color: AppColors.description),
           ),
-          const SizedBox(width: 12),
-          Flexible(
-            child: Text(
-              taskName ?? '',
-              overflow: TextOverflow.ellipsis,
-              maxLines: 3,
-            ),
+          const SizedBox(height: 8),
+          GetGoalGradientText(
+            taskName ?? '',
+            style: bodyBold(),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            taskDesc ?? '',
+            style: subHeadlineRegular(),
           ),
         ],
       ),
