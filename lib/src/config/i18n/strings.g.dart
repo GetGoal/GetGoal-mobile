3d846b30009560ea5bf4606a957aa2225f472906
xbklyn <nut.chotiwit@gmail.com>
2024-03-24 05:34:29 +0700

    add required dependencies

--- lib/src/config/i18n/strings.g.dart
+++ lib/src/config/i18n/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 2
 /// Strings: 126 (63 per locale)
 ///
-/// Built on 2024-03-20 at 08:42 UTC
+/// Built on 2024-03-23 at 19:05 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint
